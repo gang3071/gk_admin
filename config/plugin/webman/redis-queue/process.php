7ec788f234076b596f73bc6c9e3c76d7fb4177de
工作 <licheng6256@tutamail.com>
2026-03-19 15:26:21 +0800

    后管调整

--- config/plugin/webman/redis-queue/process.php
+++ config/plugin/webman/redis-queue/process.php
@@ -1,5 +1,7 @@
 <?php
 return [
+    // 已禁用，项目使用 ex-admin 的消费者进程
+    /*
     'consumer'  => [
         'handler'     => Webman\RedisQueue\Process\Consumer::class,
         'count'       => 8, // 可以设置多进程同时消费
@@ -8,4 +10,5 @@ return [
             'consumer_dir' => app_path() . '/queue/redis'
         ]
     ]
+    */
 ];
