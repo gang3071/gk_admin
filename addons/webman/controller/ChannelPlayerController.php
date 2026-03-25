28c2502a3db5ef1cec24c7a5ddda6fb7d2a838b3
工作 <licheng6256@tutamail.com>
2026-03-26 00:29:32 +0800

    設備列表電子遊戲禁用拿掉 設備列表開分拿掉 電子遊戲管理遊戲紀錄報錯 開洗分中心篩選列表修改

--- addons/webman/controller/ChannelPlayerController.php
+++ addons/webman/controller/ChannelPlayerController.php
@@ -4571,21 +4571,25 @@ class ChannelPlayerController
             })->align('center')->width('80px');
 
             $grid->actions(function (Actions $actions, Game $data) use ($player_id, $selectedGameIds) {
+                $actions->hideDel();
+                $actions->hideEdit();
+
                 // 判断当前游戏是否被禁用
                 $isDisabled = in_array($data->id, $selectedGameIds);
-                // 已禁用，显示"取消禁用"按钮
-                $actions->prepend(
-                    Button::create('取消禁用')
-                        ->type('default')
-                        ->size('small')
-                        ->confirm('确认取消禁用该游戏？', [$this, 'toggleGameDisable'], [
-                            'player_id' => $player_id,
-                            'game_id' => $data->id,
-                            'action' => 'enable'
-                        ])
-                        ->gridRefresh());
-                if ($isDisabled) {
 
+                if ($isDisabled) {
+                    // 已禁用，显示"取消禁用"按钮
+                    $actions->prepend(
+                        Button::create('取消禁用')
+                            ->type('default')
+                            ->size('small')
+                            ->confirm('确认取消禁用该游戏？', [$this, 'toggleGameDisable'], [
+                                'player_id' => $player_id,
+                                'game_id' => $data->id,
+                                'action' => 'enable'
+                            ])
+                            ->gridRefresh()
+                    );
                 } else {
                     // 未禁用，显示"禁用游戏"按钮
                     $actions->prepend(
