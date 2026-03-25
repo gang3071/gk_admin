206c721a5947c469e5d23c7daf0cfe570b751b5b
工作 <licheng6256@tutamail.com>
2026-03-25 16:29:28 +0800

    导出文件调整

--- addons/webman/controller/StoreShiftHandoverRecordController.php
+++ addons/webman/controller/StoreShiftHandoverRecordController.php
@@ -49,12 +49,16 @@ class StoreShiftHandoverRecordController
                     Html::div()->content('开始：' . $data['start_time']),
                     Html::div()->content('结束：' . $data['end_time'])
                 ]);
+            })->exportValue(function ($val, $data) {
+                return $data['start_time'] . ' ~ ' . $data['end_time'];
             })->width(200);
 
             $grid->column('is_auto_shift', '交班类型')->display(function ($value) {
                 return $value == 1
                     ? Tag::create('自动交班')->color('blue')
                     : Tag::create('手动交班')->color('default');
+            })->exportValue(function ($value) {
+                return $value == 1 ? '自动交班' : '手动交班';
             })->width(100)->align('center');
 
             $grid->column('machine_point', '投钞点数')->width(100)->align('center');
@@ -65,6 +69,8 @@ class StoreShiftHandoverRecordController
                 ->display(function ($value) {
                     $color = $value >= 0 ? '#3f8600' : '#cf1322';
                     return Html::create($value)->style(['color' => $color]);
+                })->exportValue(function ($value) {
+                    return $value;
                 });
 
             $grid->column('created_at', '创建时间')->width(180)->align('center');
@@ -81,7 +87,7 @@ class StoreShiftHandoverRecordController
                         ]),
                         ['shift_record_id' => $data['id']]
                     )->width('80%');
-            })->width(120)->align('center');
+            })->width(120)->align('center')->hideInExport();
 
             // 行展开 - 显示详细信息
             $grid->expandRow(function ($row) {
