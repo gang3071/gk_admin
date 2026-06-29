0edea9a7004d9ca8154d0cc41d75ef5a913265f2
工作 <licheng6256@tutamail.com>
2026-06-29 21:01:40 +0800

    百家禁用调整

--- addons/webman/lang/en/channel_player.php
+++ addons/webman/lang/en/channel_player.php
@@ -8,10 +8,22 @@ return [
         'player_not_found' => 'Player not found',
         'offline_channel_only' => 'This feature is only available for offline channels',
         'no_game_platform' => 'This channel has not enabled any gaming platforms',
+        'platform_not_found' => 'Gaming platform not found',
+        'platform_not_in_channel' => 'This platform is not in the channel configuration',
     ],
 
     // Game permission management
     'game_permission' => [
         'title' => 'Player Game Permission Management - {name}',
     ],
+
+    // Platform disable management
+    'platform_disable' => 'Live Casino Disable',
+    'platform_disable_title' => 'Live Casino Disable - {name}',
+    'platform_name' => 'Platform Name',
+    'game_status' => 'Game Status',
+    'status_normal' => 'Normal',
+    'status_disabled' => 'Disabled',
+    'status_not_disabled' => 'Not Disabled',
+    'disabled_status' => 'Disabled Status',
 ];
