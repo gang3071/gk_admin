b0f73b897a36acd8bd1f9ec91c7ec977cf12f504
codelin <494020937@qq.com>
2026-09-09 14:50:16 +0800

    优化店家分润报表展示

--- addons/webman/controller/AgentStoreProfitReportController.php
+++ addons/webman/controller/AgentStoreProfitReportController.php
@@ -131,8 +131,8 @@ class AgentStoreProfitReportController
             $redeemMachineAmount = floatval($redeemData->redeem_machine_amount ?? 0);
 
             $incomingTicketAmount = bcadd($ticketOpenScoreUsedAmount, $redeemMachineAmount, 2);
-            // 未核销 = 出卷 - 后台核销 - 机台核销
-            $totalRedeem = bcadd($redeemAmount, $redeemMachineAmount, 2);
+            // 未核销 = 出卷 - 后台核销 - 机台核销 - 柜台核销
+            $totalRedeem = bcadd(bcadd($redeemAmount, $redeemMachineAmount, 2), $counterRedeemAmount, 2);
             $ticketUnredeemedAmount = bcsub($ticketRedeemAmount, $totalRedeem, 2);
 
             // 拉彩数据
--- addons/webman/controller/ChannelStoreProfitReportController.php
+++ addons/webman/controller/ChannelStoreProfitReportController.php
@@ -165,8 +165,8 @@ class ChannelStoreProfitReportController
 
             // 入票 = 开票机台使用 + 核销机台使用
             $incomingTicketAmount = bcadd($ticketOpenScoreUsedAmount, $redeemMachineAmount, 2);
-            // 未核销 = 出卷 - 后台核销 - 机台核销
-            $totalRedeem = bcadd($redeemAmount, $redeemMachineAmount, 2);
+            // 未核销 = 出卷 - 后台核销 - 机台核销 - 柜台核销
+            $totalRedeem = bcadd(bcadd($redeemAmount, $redeemMachineAmount, 2), $counterRedeemAmount, 2);
             $ticketUnredeemedAmount = bcsub($ticketRedeemAmount, $totalRedeem, 2);
 
             // 拉彩数据
