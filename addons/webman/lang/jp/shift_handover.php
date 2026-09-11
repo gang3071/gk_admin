e41a5910f70b3589b42dba9e816a18293b2ed646
codelin <494020937@qq.com>
2026-09-11 16:13:32 +0800

    店机中心當前班次實時統計 增加储值机储值和储值机购票

--- addons/webman/lang/jp/shift_handover.php
+++ addons/webman/lang/jp/shift_handover.php
@@ -30,6 +30,8 @@ return [
     'electronic_game_bet_amount' => '電子ゲームベット額',
     'machine_bet_amount' => 'マシンベット額',
     'ticket_redeem_current_shift' => '出票・精算統計（現在のシフト）',
+    'storage_recharge' => 'チャージ機チャージ',
+    'storage_ticket_purchase' => 'チャージ機チケット購入',
     'ticket_record_total_score' => '出票記録合計金額',
     'ticket_redeem_backend_used_score' => '精算バックエンド使用金額',
     'ticket_subtotal' => '小計',
